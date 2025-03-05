192b901d93d9f023c8e4c90660ab95db93f9a1a0
Rahat Islam Akash <rahatislamakash@gmail.com>
2025-03-06 03:32:14 +0600

    setting up the theme data for future reference

--- lib/app/view/theme/theme_cubit.dart
+++ lib/app/view/theme/theme_cubit.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:truth_box/app/view/theme/app_colors.dart';
 import 'package:truth_box/app/view/theme/app_theme.dart';
+import 'package:truth_box/app/view/theme/dark_theme/dark_theme.dart';
 import 'package:truth_box/app/view/theme/light_theme/light_theme.dart';
 
 extension ThemeExtension on BuildContext {
@@ -13,9 +15,24 @@ extension ThemeExtension on BuildContext {
 }
 
 class ThemeCubit extends Cubit<AppTheme> {
-  ThemeCubit() : super(LightTheme());
+  ThemeCubit() : super(LightTheme()) {
+    _loadTheme();
+  }
 
-  void changeTheme(AppTheme appTheme) {
+  Future<void> changeTheme(AppTheme appTheme) async {
+    final preference = await SharedPreferences.getInstance();
+    await preference.setString('theme', appTheme.runtimeType.toString());
     emit(appTheme);
+  }
+
+  Future<void> _loadTheme() async {
+    final preference = await SharedPreferences.getInstance();
+    final theme = preference.getString('theme');
+    switch (theme) {
+      case 'LightTheme':
+        emit(LightTheme());
+      case 'DarkTheme':
+        emit(DarkTheme());
+    }
   }
 }
