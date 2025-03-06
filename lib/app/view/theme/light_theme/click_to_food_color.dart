34dff913abf4b36cf40ae1e12dd36df9cc06485f
Rahat Islam Akash <rahatislamakash@gmail.com>
2025-03-06 22:18:52 +0600

    new color added

---
+++ lib/app/view/theme/light_theme/click_to_food_color.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ProdAppColor {
+  //background color =================
+  static const Color backgroundColor = Color(0XFFFFFFFF);
+  //primary color ====================
+  static const Color primaryColor45A = Color(0XFFA1045A);
+  static const Color primary2ndColorE5D = Color(0XFFA01E5D);
+  static const Color secondaryColor = Color(0XFF000000);
+  //shade and text color =====================
+  static const Color shadeColorGray200 = Color(0XFFC6C9CD);
+  static const Color shadeColorGray400 = Color(0XFF90969D);
+  static const Color shadeColorGray500 = Color(0XFF757D85);
+  static const Color shadeColorGray700 = Color(0XFF5D636A);
+  static const Color shadeColorGray900 = Color(0XFF2D3034);
+  static const Color shadeColorGray9D9 = Color(0XFFD9D9D9);
+  //other color ==============================
+  static const Color inactiveColor = Color(0XFFABB0B5);
+  static const Color worningTextColor = Color(0XFFF1160D);
+  static const Color yellowColor = Color(0XFFF8BD00);
+  static const Color worningColor629 = Color(0XFFE33629);
+  static const Color successColorF43 = Color(0XFF319F43);
+  static const Color sayanColorDBD = Color(0XFF587DBD);
+  static const Color darkColor = Color(0XFF030401);
+  static const Color bannerColor503 = Color(0XFFFB4503);
+  static const Color bannerColor000 = Color(0XFFEB0000);
+  static const Color remarkColor = Color(0XFFEB0000);
+  static const Color yellow9CD = Color(0XFFFEE9CD);
+  static const Color outlinedColor = Color(0XFFFAF2F7);
+  static const Color greenColorA9D = Color(0XFF95EA9D);
+  static const Color greenColor066 = Color(0XFF25C066);
+  static const Color starColor = Color(0XFFFFA133);
+}
